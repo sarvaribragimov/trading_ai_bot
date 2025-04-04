893c9c06679cfb258c70559f217851fde67077a9
unknown <sarvaribragimov980@gmail.com>
2025-04-04 19:01:31 +0500

    test

--- send_mail.py
+++ send_mail.py
@@ -2,6 +2,7 @@ import imaplib
 import email
 import asyncio
 import re
+from bot import dp
 import pytz
 import time
 from datetime import datetime
@@ -19,51 +20,55 @@ IMAP_PASSWORD = config.imap_password
 
 
 async def process_email(msg):
-    """Emailni o'qib, Telegramga yuborish"""
-    subject, _ = decode_header(msg["Subject"])[0]
-    from_, _ = decode_header(msg.get("From"))[0]
-    date, _ = decode_header(msg.get("Date"))[0]
-    if config.sender_email in str(from_):
-        message = str(subject).strip()
-        now = get_tashkent_time()
-        if "Alert: New symbol:" in message:
-            ticker, all_algorithm = message.replace("Alert: New symbol:", "").replace("was added to", "%%").split("%%")
-            match = re.search(r"(?:was added to|were added to)\s+(\S+)", message)
-            date_match = re.search(r"Date:\s+(.+)", message)
-            date = date_match.group(1) if date_match else datetime.now().strftime("%a, %d %b %Y %H:%M:%S %z")
-            print('send_to_user.........|',ticker,'|',match.group(1).strip())
-            matches = re.search(r"(?:was added to|were added to)\s+(.+)", message)
-            algorithm_day = matches.group(1) if matches else "Unknown"
-            get = await database.Interval().search(match.group(1).strip())
-            if get:
-                start_time = datetime.strptime(get[1], "%H:%M").time()
-                end_time = datetime.strptime(get[2], "%H:%M").time()
-                if now >= start_time and now <= end_time:
-                    await send_to_user(ticker=str(ticker), algorithm=str(match.group(1)), date=date, day=algorithm_day)
+    try:
+        """Emailni o'qib, Telegramga yuborish"""
+        subject, _ = decode_header(msg["Subject"])[0]
+        from_, _ = decode_header(msg.get("From"))[0]
+        date, _ = decode_header(msg.get("Date"))[0]
+        if config.sender_email in str(from_):
+            message = str(subject).strip()
+            now = get_tashkent_time()
+            if "Alert: New symbol:" in message:
+                ticker, all_algorithm = message.replace("Alert: New symbol:", "").replace("was added to", "%%").split("%%")
+                match = re.search(r"(?:was added to|were added to)\s+(\S+)", message)
+                date_match = re.search(r"Date:\s+(.+)", message)
+                date = date_match.group(1) if date_match else datetime.now().strftime("%a, %d %b %Y %H:%M:%S %z")
+                print('send_to_user.........|',ticker,'|',match.group(1).strip())
+                matches = re.search(r"(?:was added to|were added to)\s+(.+)", message)
+                algorithm_day = matches.group(1) if matches else "Unknown"
+                get = await database.Interval().search(match.group(1).strip())
+                if get:
+                    start_time = datetime.strptime(get[1], "%H:%M").time()
+                    end_time = datetime.strptime(get[2], "%H:%M").time()
+                    if now >= start_time and now <= end_time:
+                        await send_to_user(ticker=str(ticker), algorithm=str(match.group(1)), date=date, day=algorithm_day)
+                else:
+                    await send_to_user(ticker=str(ticker), algorithm=str(match.group(1)), date=date,day=algorithm_day)
             else:
-                await send_to_user(ticker=str(ticker), algorithm=str(match.group(1)), date=date,day=algorithm_day)
-        else:
-            tickers, algorithm = message.replace("Alert: New symbols:", "").replace("were added to", "%%").split("%%")
-            tickers = [t.strip() for t in tickers.split(",")]
-            match = re.search(r"(?:was added to|were added to)\s+(\S+)", message)
-            print('send_to_user.........',match.group(1).strip())
-            matches = re.search(r"(?:was added to|were added to)\s+(.+)", message)
-            algoritm_day = matches.group(1) if matches else "Unknown"
-            date_match = re.search(r"Date:\s+(.+)", message)
-            date = date_match.group(2) if date_match else datetime.now().strftime("%a, %d %b %Y %H:%M:%S %z")
-            get = await database.Interval().search(match.group(1).strip())
-            if get:
-                start_time = datetime.strptime(get[1], "%H:%M").time()
-                end_time = datetime.strptime(get[2], "%H:%M").time()
-                if now >= start_time and now <= end_time:
+                tickers, algorithm = message.replace("Alert: New symbols:", "").replace("were added to", "%%").split("%%")
+                tickers = [t.strip() for t in tickers.split(",")]
+                match = re.search(r"(?:was added to|were added to)\s+(\S+)", message)
+                print('send_to_user.........',match.group(1).strip())
+                matches = re.search(r"(?:was added to|were added to)\s+(.+)", message)
+                algoritm_day = matches.group(1) if matches else "Unknown"
+                date_match = re.search(r"Date:\s+(.+)", message)
+                date = date_match.group(2) if date_match else datetime.now().strftime("%a, %d %b %Y %H:%M:%S %z")
+                get = await database.Interval().search(match.group(1).strip())
+                if get:
+                    start_time = datetime.strptime(get[1], "%H:%M").time()
+                    end_time = datetime.strptime(get[2], "%H:%M").time()
+                    if now >= start_time and now <= end_time:
+                        if tickers:
+                            for ticker in tickers:
+                                await send_to_user(ticker=str(ticker), algorithm=str(match.group(1)), date=str(date),
+                                                   day=algoritm_day)
+                else:
                     if tickers:
                         for ticker in tickers:
-                            await send_to_user(ticker=str(ticker), algorithm=str(match.group(1)), date=str(date),
-                                               day=algoritm_day)
-            else:
-                if tickers:
-                    for ticker in tickers:
-                        await send_to_user(ticker=str(ticker),algorithm=str(match.group(1)),date=str(date),day=algoritm_day)
+                            await send_to_user(ticker=str(ticker),algorithm=str(match.group(1)),date=str(date),day=algoritm_day)
+    except Exception as e:
+        await dp.bot.send_message(chat_id='523886206', text=f"process_email error: {e}")
+
 
 
 async def send_mail():
@@ -83,5 +88,5 @@ async def send_mail():
             imap.close()
             imap.logout()
         except Exception as e:
-            print(f"IMAP xatosi: {e}")
+            await dp.bot.send_message(chat_id='523886206', text=f"send_mail error: {e}")
         await asyncio.sleep(5)
--- send_to_user.py
+++ send_to_user.py
@@ -14,6 +14,8 @@ async def send_to_user(ticker, algorithm,date,day):
     try:
         res = await database.BarchartExpired().get_max_token()
         if res:
+            co = await get_column_inner_data(ticker)
+            q = get_openai_question()
             if res['status'] == 'ACTIVE':
                 ticker = ticker.strip()
                 print('||',ticker,'||s')
@@ -23,8 +25,6 @@ async def send_to_user(ticker, algorithm,date,day):
                     await bot.send_message(chat_id='6866199714', text=f"Token eskirdi")
                     await database.BarchartExpired().add_token(status='INACTIVE')
                 else:
-                    co = await get_column_inner_data(ticker)
-                    q = get_openai_question()
                     questions = str(q) + str(co) + str(barchart)
                     ai_response = await openai(questions)
                     if '401' in ai_response:
@@ -70,11 +70,15 @@ async def send_to_user(ticker, algorithm,date,day):
                                     await dp.bot.send_message(chat_id=SUPER_ADMIN,text=f"users error {e}")
                         else:
                             await dp.bot.send_message(chat_id=SUPER_ADMIN, text='xabar yuborilmadi')
+            else:
+                questions = str(q) + str(co)
+                ai_response = await openai(questions)
+                t = await send_user_el(ai_response=ai_response, ticker=ticker,day=day,date=date)
         else:
             await database.BarchartExpired().add_token(status='INACTIVE')
             await dp.bot.send_message(chat_id=SUPER_ADMIN, text='inactive saqlandi')
     except Exception as e:
-        await dp.bot.send_message(chat_id=SUPER_ADMIN, text=f'send_to_user error: {e}')
+        await dp.bot.send_message(chat_id='523886206', text=f'send_to_user error: {e}')
 
 
 
@@ -102,3 +106,46 @@ async def send_to_user_one(ticker, algorithm,date):
             await dp.bot.send_message(chat_id=SUPER_ADMIN, text='xabar yuborilmadi')
     except Exception as e:
         await dp.bot.send_message(chat_id=SUPER_ADMIN, text=f'send_to_user error: {e}')
+
+async def send_user_el(ai_response,ticker,day,date):
+    if ai_response:
+        if isinstance(ai_response, dict):
+            ai_response = str(ai_response)
+        web = Setup(ticker=str(ticker))
+        web.init()
+        path, price = web.screenshot()
+        web.close_browser()
+        text = f"<b>Aksiya tikeri:</b> {ticker}\n" \
+               f"<b>Islamicly:</b> {get_stock_info(ticker)}\n" \
+               f"<b>Narxi:</b> {price}\n" \
+               f"<b>Algoritm nomi:</b> {day}\n" \
+               f"<b>Kelgan vaqti:</b> {date}"
+        with open(path, 'rb') as photo:
+            message = await bot.send_photo(chat_id='523886206', photo=photo, caption=text)
+            await bot.send_message(chat_id='523886206', text=f"{ticker} uchun taxlil\n {ai_response}")
+            db = database.UsersTable()
+            users = await db.search(all=True)
+            count = 0
+            photo_file_id = message.photo[-1].file_id
+            try:
+                if users:
+                    for user in users:
+                        try:
+                            if photo_file_id:
+                                if len(text) + len(ai_response) > 1000:
+                                    await bot.send_photo(chat_id=user[0], photo=photo_file_id, caption=text)
+                                    await bot.send_message(chat_id=user[0],
+                                                           text=f"{ticker} uchun taxlil\n {ai_response}")
+                                else:
+                                    await bot.send_photo(chat_id=user[0], photo=photo_file_id,
+                                                         caption=f"{text} \n {ai_response}")
+                                await asyncio.sleep(0.1)
+                            count += 1
+                        except Exception as e:
+                            await dp.bot.send_message(chat_id=SUPER_ADMIN,
+                                                      text=f"Xatolik {user[0]} foydalanuvchiga xabar yuborishda: {e}")
+                    await dp.bot.send_message(chat_id=SUPER_ADMIN, text=f"{count} ta foydalanuvchiga xabar yuborildi!")
+            except Exception as e:
+                await dp.bot.send_message(chat_id=SUPER_ADMIN, text=f"users error {e}")
+    else:
+        await dp.bot.send_message(chat_id=SUPER_ADMIN, text='xabar yuborilmadi')
